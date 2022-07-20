3fda8bacb9c56fdd4291e693ce536967339aa2d3
mrrs878 <mrrs878@foxmail.com>
2022-07-20 20:35:01 +0800

    chore: fix lint

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,7 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-12-07 20:50:34
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2022-07-20 19:43:01
+ * @LastEditTime: 2022-07-20 20:31:30
  * @FilePath: \checklist\js\popup.js
  */
 
@@ -19,20 +19,25 @@ const selectors = {
 const TaskState = {
   pending: 0,
   resolved: 1,
-}
+};
 
 function renderTaskTitle(task, index) {
   const { state, title } = task;
-  if (state === TaskState.pending) return (`
+  if (state === TaskState.pending) {
+    return (`
     <label class="custom-control-label" id="customCheckLabel${index}" for="customCheck${index}">
       ${title}
     </label>
   `);
-  if (state === TaskState.resolved) return (`
+  }
+  if (state === TaskState.resolved) {
+    return (`
     <label class="custom-control-label task-resolved" id="customCheckLabel${index}" for="customCheck${index}">
       ${title}
     </label>
   `);
+  }
+  return '';
 }
 
 function renderTaskState(task, index) {
@@ -64,6 +69,18 @@ function renderTaskAction(task, index) {
   `);
 }
 
+function renderTask(task, index) {
+  return (`
+    <li class="list-group-item list-group-item-action task-item" id="taskItem${index}">
+      <div class="custom-control custom-checkbox">
+        ${renderTaskState(task, index)}
+        ${renderTaskTitle(task, index)}
+      </div>
+      ${renderTaskAction(task, index)}
+    </li>
+  `);
+}
+
 function getTasks() {
   return new Promise((resolve) => {
     chrome.storage.sync.get((storage) => {
@@ -76,7 +93,7 @@ async function addTask(task) {
   const tasks = await getTasks();
   tasks.push(task);
   chrome.storage.sync.set({
-    [storageKey]: tasks
+    [storageKey]: tasks,
   });
   return tasks.length - 1;
 }
@@ -85,7 +102,7 @@ async function deleteTask(taskIndex) {
   let tasks = await getTasks();
   tasks = tasks.filter((item, index) => index !== taskIndex);
   chrome.storage.sync.set({
-    [storageKey]: tasks
+    [storageKey]: tasks,
   });
 }
 
@@ -93,35 +110,8 @@ async function copyTask(taskIndex) {
   const tasks = await getTasks();
   tasks.push(tasks[taskIndex]);
   chrome.storage.sync.set({
-    [storageKey]: tasks
+    [storageKey]: tasks,
   });
-}
-
-async function toggleTaskState(taskIndex) {
-  const tasks = await getTasks();
-  if (tasks.length === 0) return;
-
-  const { state: preState } = tasks[taskIndex];
-  const newState = preState === TaskState.pending ? TaskState.resolved : TaskState.pending;
-  tasks[taskIndex].state = newState;
-
-  chrome.storage.sync.set({
-    [storageKey]: tasks
-  });
-
-  await renderTasks();
-}
-
-function renderTask(task, index) {
-  return (`
-    <li class="list-group-item list-group-item-action task-item" id="taskItem${index}">
-      <div class="custom-control custom-checkbox">
-        ${renderTaskState(task, index)}
-        ${renderTaskTitle(task, index)}
-      </div>
-      ${renderTaskAction(task, index)}
-    </li>
-  `)
 }
 
 function setBadge(tasks) {
@@ -142,8 +132,8 @@ async function renderTasks() {
       </p>
     `);
     return;
-  };
-  
+  }
+
   const content = tasks.reduce((pre, cur, index) => `
     ${pre}
     ${renderTask(cur, index)}
@@ -153,6 +143,7 @@ async function renderTasks() {
   $('input[type=checkbox]').each((index, input) => {
     $(input).on('change', async () => {
       console.log('toggle', index);
+      // eslint-disable-next-line no-use-before-define
       toggleTaskState(index);
     });
   });
@@ -163,7 +154,7 @@ async function renderTasks() {
       await deleteTask(index);
       await renderTasks();
     });
-  })
+  });
 
   $('.task-item .copy-icon').each((index, copyIcon) => {
     $(copyIcon).on('click', async () => {
@@ -171,7 +162,22 @@ async function renderTasks() {
       await copyTask(index);
       await renderTasks();
     });
-  })
+  });
+}
+
+async function toggleTaskState(taskIndex) {
+  const tasks = await getTasks();
+  if (tasks.length === 0) return;
+
+  const { state: preState } = tasks[taskIndex];
+  const newState = preState === TaskState.pending ? TaskState.resolved : TaskState.pending;
+  tasks[taskIndex].state = newState;
+
+  chrome.storage.sync.set({
+    [storageKey]: tasks,
+  });
+
+  await renderTasks();
 }
 
 window.addEventListener('load', async () => {
@@ -181,28 +187,28 @@ window.addEventListener('load', async () => {
     const form = document.querySelector('.needs-validation');
     form.addEventListener('submit', async (event) => {
       if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
+        event.preventDefault();
+        event.stopPropagation();
       } else {
         const taskTitle = $(selectors.addTaskTitleInput).prop('value');
         const newTask = {
           title: taskTitle,
           state: TaskState.pending,
-        }
+        };
         await addTask(newTask);
 
         $(selectors.addTaskTitleInput).prop('value', '');
         renderTasks();
       }
 
-      form.classList.add('was-validated')
+      form.classList.add('was-validated');
     }, false);
   });
 
-  $(selectors.editTaskModal).on('show.bs.modal', async function (event) {
+  $(selectors.editTaskModal).on('show.bs.modal', async (event) => {
     const { index } = event.relatedTarget?.dataset || {};
     const taskIndex = parseInt(index, 10);
-    if (isNaN(taskIndex)) return;
+    if (Number.isNaN(taskIndex)) return;
 
     const tasks = await getTasks();
     const { title } = tasks[taskIndex] || {};
@@ -215,7 +221,7 @@ window.addEventListener('load', async () => {
   $(selectors.editTaskModalSubmitBtn).on('click', async () => {
     const index = $(selectors.editTaskModal).data('editTaskIndex');
     const editTaskIndex = parseInt(index, 10);
-    if (isNaN(editTaskIndex)) return;
+    if (Number.isNaN(editTaskIndex)) return;
 
     const tasks = await getTasks();
     const task = tasks[editTaskIndex];
@@ -224,11 +230,11 @@ window.addEventListener('load', async () => {
     task.title = $(selectors.editTaskTitleInput).val();
 
     chrome.storage.sync.set({
-      [storageKey]: tasks
+      [storageKey]: tasks,
     });
 
     await renderTasks();
 
     $(selectors.editTaskModal).modal('hide');
   });
-})
+});
